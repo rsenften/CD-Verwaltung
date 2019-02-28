f52ceaed150ae9e9cf8ed780a0b337f2f974d3d0
vmadmin <vmadmin@BMWP2>
2019-02-28 11:22:18 +0100

    First Commit

---
+++ M120Projekt/APIDemo.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M120Projekt
+{
+    static class APIDemo
+    {
+        #region KlasseA
+        // Create
+        public static void DemoACreate()
+        {
+            Debug.Print("--- DemoACreate ---");
+            // KlasseA
+            Data.CD klasseA1 = new Data.CD();
+            klasseA1.CDName = "Artikel 1";
+            klasseA1.Erstellung = DateTime.Today;
+            klasseA1.Lied = "Lied 1";
+            klasseA1.Film = "Film 1";
+            klasseA1.Kuenstler = "Künstler 1";
+            klasseA1.Produzent = "Produzent";
+            klasseA1.Dauer = new TimeSpan(0, 3, 25);
+            klasseA1.Erstellung = DateTime.Today;
+            klasseA1.Veroeffentlichung = DateTime.Today.AddMonths(-1);
+            klasseA1.IstIntakt = true;
+            Int64 klasseA1Id = klasseA1.Erstellen();
+            Debug.Print("Artikel erstellt mit Id:" + klasseA1Id);
+        }
+        public static void DemoACreateKurz()
+        {
+            Data.CD klasseA2 = new Data.CD { CDName = "Artikel 2", IstIntakt = true, Erstellung = DateTime.Today, Lied = "Lied 2", Film = "Film 2", Kuenstler = "Künstler 2", Produzent = "Produzent 2", Dauer = new TimeSpan(0, 4, 15), Veroeffentlichung = DateTime.Today.AddYears(-1).AddMonths(-5) };
+            Int64 klasseA2Id = klasseA2.Erstellen();
+            Debug.Print("Artikel erstellt mit Id:" + klasseA2Id);
+        }
+
+        // Read
+        public static void DemoARead()
+        {
+            Debug.Print("--- DemoARead ---");
+            // Demo liest alle
+            foreach (Data.CD klasseA in Data.CD.LesenAlle())
+            {
+                Debug.Print("Artikel Id:" + klasseA.CDId + " Name:" + klasseA.CDName);
+            }
+        }
+        // Update
+        public static void DemoAUpdate()
+        {
+            Debug.Print("--- DemoAUpdate ---");
+            // KlasseA ändert Attribute
+            Data.CD klasseA1 = Data.CD.LesenID(1);
+            klasseA1.CDName = "Artikel 1 nach Update";
+            klasseA1.Aktualisieren();
+        }
+        // Delete
+        public static void DemoADelete()
+        {
+            Debug.Print("--- DemoADelete ---");
+            Data.CD.LesenID(1).Loeschen();
+            Debug.Print("Artikel mit Id 1 gelöscht");
+        }
+        #endregion
+    }
+}
---
+++ M120Projekt/Data/CD.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+namespace M120Projekt.Data
+{
+    public class CD
+    {
+        #region Datenbankschicht
+        [Key]
+        public Int64 CDId { get; set; }
+        [Required]
+        public String CDName { get; set; }
+        [Required]
+        public String Lied { get; set; }
+        [Required]
+        public String Film { get; set; }
+        [Required]
+        public String Kuenstler { get; set; }
+        [Required]
+        public String Produzent { get; set; }
+        [Required]
+        public TimeSpan Dauer { get; set; }
+        [Required]
+        public DateTime Erstellung { get; set; }
+        [Required]
+        public DateTime Veroeffentlichung { get; set; }
+        public Boolean IstIntakt { get; set; }
+        #endregion
+        #region Applikationsschicht
+        public CD() { }
+        [NotMapped]
+        public String BerechnetesAttribut
+        {
+            get
+            {
+                return "Im Getter kann Code eingefügt werden für berechnete Attribute";
+            }
+        }
+        public static IEnumerable<Data.CD> LesenAlle()
+        {
+            return (from record in Data.Global.context.CD select record);
+        }
+        public static Data.CD LesenID(Int64 klasseAId)
+        {
+            return (from record in Data.Global.context.CD where record.CDId == klasseAId select record).FirstOrDefault();
+        }
+        public static IEnumerable<Data.CD> LesenAttributGleich(String suchbegriff)
+        {
+            return (from record in Data.Global.context.CD where record.CDName == suchbegriff select record);
+        }
+        public static IEnumerable<Data.CD> LesenAttributWie(String suchbegriff)
+        {
+            return (from record in Data.Global.context.CD where record.CDName.Contains(suchbegriff) select record);
+        }
+        public Int64 Erstellen()
+        {
+            if (this.CDName == null || this.CDName == "") this.CDName = "leer";
+            if (this.Erstellung == null) this.Erstellung = DateTime.MinValue;
+            Data.Global.context.CD.Add(this);
+            Data.Global.context.SaveChanges();
+            return this.CDId;
+        }
+        public Int64 Aktualisieren()
+        {
+            Data.Global.context.Entry(this).State = System.Data.Entity.EntityState.Modified;
+            Data.Global.context.SaveChanges();
+            return this.CDId;
+        }
+        public void Loeschen()
+        {
+            Data.Global.context.Entry(this).State = System.Data.Entity.EntityState.Deleted;
+            Data.Global.context.SaveChanges();
+        }
+        public override string ToString()
+        {
+            return CDId.ToString(); // Für bessere Coded UI Test Erkennung
+        }
+        #endregion
+    }
+}
